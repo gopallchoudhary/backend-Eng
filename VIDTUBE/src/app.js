c3574ad8fbeaed51acd0b58a3f6d6f25571e22de
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-12-03 18:53:26 +0530

    setup the database connection

---
+++ VIDTUBE/src/app.js
@@ -0,0 +1,16 @@
+import express, { urlencoded } from "express";
+import cors from "cors";
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+//? common middleware
+app.use(express.json({ limit: "16kb" }));
+app.use(urlencoded({ extended: true, limit: "16bk" }));
+app.use(express.static("public"));
+
+export { app };
