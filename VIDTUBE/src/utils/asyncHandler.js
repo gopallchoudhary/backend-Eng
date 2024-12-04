2560781e5df66d4e34126f4ced3fa7a261768054
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-12-04 08:21:35 +0530

    added error handler

--- VIDTUBE/src/utils/asyncHandler.js
+++ VIDTUBE/src/utils/asyncHandler.js
@@ -1,8 +1,11 @@
-const asyncHandler = (requestHandler) =>  {
+const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
-        Promise.resolve(asyncHandler(req, res, next)).catch
+        Promise.resolve(requestHandler(req, res, next)).catch
         ((err) => next(err))
     }
 }
 
 export {asyncHandler}
+
+//? asyncHandler is higher order function, it is used for wrapping up the function so  that it can reusable for trycatch so we dont have to write try catch multiple times
+//? asyncHandler is just a method
