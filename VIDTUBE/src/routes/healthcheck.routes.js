dfd8cc17632dfbe1b0f2db43d5e2adfd7643ee4c
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-12-04 08:20:42 +0530

    added healthcheck routes

---
+++ VIDTUBE/src/routes/healthcheck.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { healthcheck } from "../controllers/healthcheck.controller.js";
+
+const router = Router()
+
+router.route("/").get(healthcheck)
+
+export default router;
+
+//? router.route("/test").get(healthcheck) => "api/v1/healthcheck/test"
