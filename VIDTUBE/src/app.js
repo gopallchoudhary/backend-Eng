2d8f03f35792a1cfb562544549e75c6cfefe5da9
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-12-04 08:22:15 +0530

    added healthcheck routes

--- VIDTUBE/src/app.js
+++ VIDTUBE/src/app.js
@@ -13,4 +13,11 @@ app.use(express.json({ limit: "16kb" }));
 app.use(urlencoded({ extended: true, limit: "16bk" }));
 app.use(express.static("public"));
 
+//? import routes
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+
+
+
+//? routes
+app.use("/api/v1/healthcheck", healthcheckRouter)
 export { app };
