026f6ea435d78c242940e7f0b189d29d53b70aa1
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-12-08 13:31:09 +0530

    added user creation and authentication

--- VIDTUBE/src/app.js
+++ VIDTUBE/src/app.js
@@ -1,4 +1,5 @@
 import express, { urlencoded } from "express";
+import cookieParser from "cookie-parser";
 import cors from "cors";
 const app = express();
 app.use(
@@ -12,12 +13,15 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(urlencoded({ extended: true, limit: "16bk" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 //? import routes
 import healthcheckRouter from "./routes/healthcheck.routes.js";
-
-
+import userRouter from "./routes/user.routes.js";
+import { errorHandler } from "./middlewares/error.middleware.js";
 
 //? routes
-app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/users", userRouter);
+app.use(errorHandler);
 export { app };
