6a46e95384c17d77c3affc10f8dc091b20a9895a
gopallchoudhary <gopalchoudhary521@gmail.com>
2024-12-26 22:39:30 +0530

    added: update user cover image

--- VIDTUBE/src/controllers/user.controller.js
+++ VIDTUBE/src/controllers/user.controller.js
@@ -346,7 +346,31 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
   .json(new ApiResponse(200, user, "Avatar updated successfully"))
 })
 
+//. Update Cover Image 
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path
+  if(!coverImageLocalPath) {
+    throw new ApiError(400, "Cover image file is missing")
+  }
 
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+  if(!coverImage.url) {
+    throw new ApiError(400, "Error while uploading cover image")
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        coverImage: coverImage.url
+      }
+    }, {new: true}
+  ).select("-password")
+
+  return res
+  .status(200)
+  .json(new ApiResponse(200, user, "cover image updated successfully"))
+})
 
 //. read_cookie
 // const readCookie = asyncHandler(async (req, res) => {
