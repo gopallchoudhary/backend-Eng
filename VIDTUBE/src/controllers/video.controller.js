5403505099dada92dddcf09d9559e45742f8c113
gopallchoudhary <gopalchoudhary521@gmail.com>
2025-01-01 00:41:51 +0530

    updated the vidoe controller

--- VIDTUBE/src/controllers/video.controller.js
+++ VIDTUBE/src/controllers/video.controller.js
@@ -16,6 +16,54 @@ const getAllVideos = asyncHandler(async (req, res) => {
 const publishAVideo = asyncHandler(async (req, res) => {
     const { title, description} = req.body
     // TODO: get video, upload to cloudinary, create video
+    if(!(title && description)) {
+        throw new ApiError(400, "Title and description are required")
+    }
+
+    const videoLocalPath = req.files?.video?.[0]?.path;
+    const thumbnailLocalPath = req.files?.thumbnail?.[0]?.path
+
+    //? upload on cloudinary
+    let video;
+    try {
+        video = await uploadOnCloudinary(videoLocalPath)
+    } catch (error) {
+        console.log("Error in uploading video ", error);
+        throw new ApiError(400, "Error in uploading the video")
+    }
+
+    let thumbnail;
+    try {
+        thumbnail = await uploadOnCloudinary(thumbnailLocalPath)
+    } catch (error) {
+        console.log("Thumbnail not availabe ", error)
+        throw new ApiError(400, "No Thumbnail ")
+    }
+
+    //? video creation
+    try {
+        const video = await Video.create({
+            videoFile: video?.url,
+            thumbnail: thumbnail?.url || "",
+            title,
+            description,
+            
+        })
+    } catch (error) {
+        console.log("Error in creating video ", error)
+        
+        if(video) {
+            await deleteFromCloudinary(video.public_id)
+        }
+
+        if(thumbnail) {
+            await deleteFromCloudinary(thumbnail.public_id)
+        }
+
+        throw new ApiError(404, "Something went wrong in uploading a video")
+
+
+    }
 })
 
 //. Get video by Id 
